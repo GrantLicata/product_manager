33dcb1390fd9d47bf27adf55603e736bed0cc9f0
GrantLicata <grant.licata@gmail.com>
2022-10-29 11:19:37 -0700

    Navigation styled

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -3,22 +3,22 @@ import React from 'react'
 const NavBar = () => {
     return (
         <div>
-            <nav class="navbar navbar-expand-lg bg-light mb-4">
+            <nav class="navbar navbar-expand-lg bg-secondary mb-4">
                 <div class="container-fluid">
-                    <a class="navbar-brand" href="#">Product Manager</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <a class="navbar-brand text-white" href="#"><strong>Product Manager</strong></a>
+                    <button class="navbar-toggler bg-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                     </button>
                     <div class="collapse navbar-collapse" id="navbarNav">
                     <ul class="navbar-nav">
                         <li class="nav-item">
-                        <a class="nav-link active" aria-current="page" href="#">Home</a>
+                        <a class="nav-link active  text-white" aria-current="page" href="#">Home</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">TBD</a>
+                        <a class="nav-link  text-white" href="#">TBD</a>
                         </li>
                         <li class="nav-item">
-                        <a class="nav-link" href="#">TBD</a>
+                        <a class="nav-link  text-white" href="#">TBD</a>
                         </li>
                     </ul>
                     </div>
